636ab3e370741dd55cf92c34a03c2b3fb8f09eb0
contrast-zone <33665826+contrast-zone@users.noreply.github.com>
2021-10-22 20:51:30 +0200

    Add files via upload

---
+++ init-ctrl.js
@@ -0,0 +1,8 @@
+var init = {
+    "background-color": "gray",
+    "ctrl-color": "rgb(48,48,48)",
+    "text-color": "white",
+    "shadow-radius": "0",
+    "shadow-color": "rgb(255, 255, 255, 1)"
+};
+
---
+++ init-fract.js
@@ -0,0 +1,12 @@
+var init = {
+    "sexpr-depth": 2,
+    "background-color": "gray",
+    "oval-color": "rgb(208,208,208)",
+    "text-color": "black",
+    "shadow-radius": "0",
+    "shadow-color": "rgb(0, 0, 0, 1)",
+    "contents-flat-area": "0%",
+    "power-consumption-optimisation": "80%",
+    "ui-scale": "100%"
+};
+
